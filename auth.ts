033543b352d6135cd36783070e738a393cb11675
root-user <winzaw.oo@kbzbank.com>
2025-04-09 16:04:33 +0630

    setup  next auth

---
+++ auth.ts
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { signInSchema } from "./lib/zod";
+import { ZodError } from "zod";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        try {
+          let user = null;
+
+          //   const { email, password } = await signInSchema.parseAsync(
+          //     credentials
+          //   );
+
+          user = { id: "1", email: "John Doe", password: "1o1901901930" }; // Added 'id' field to match the User type
+
+          console.log("🚀 ~ authorize ~ user:", user);
+          if (!user) {
+            throw new Error("Invalid credentials");
+          }
+          return user;
+        } catch (error) {
+          if (error instanceof ZodError) {
+            // Return `null` to indicate that the credentials are invalid
+            return null;
+          }
+          throw error; // Re-throw other errors
+        }
+      },
+    }),
+  ],
+});
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,18 +1,24 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signIn } from "@/auth";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  async function handleFormAction(formData: FormData) {
+    "use server";
+    await signIn("credentials", formData);
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form action={handleFormAction} className="p-6 md:p-8">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
@@ -100,5 +106,5 @@ export function LoginForm({
         and <a href="#">Privacy Policy</a>.
       </div>
     </div>
-  )
+  );
 }
