ea5cf5e61f906fe28eca8836e41596ebe5e7a2dd
root-user <winzaw.oo@kbzbank.com>
2025-05-07 15:09:46 +0630

    commnet unused code

--- lib/auth/cognito-auth-provider.ts
+++ lib/auth/cognito-auth-provider.ts
@@ -14,166 +14,166 @@ type ForgotPassword = Pick<SignUpCredential, 'email'> & { onSuccess: (data: any)
 
 export type ConfirmForgotPassword = Pick<SignUpCredential, 'email'> & { otpCode: string; newPassword: string; onSuccess: (success: string) => void; onFailure: (error: Error) => void; }
 
-const UserPoolId = process.env.AWS_COGNITO_USER_POOL_ID || '';
-const ClientId = process.env.AWS_COGNITO_CLIENT_ID || '';
+// const UserPoolId = process.env.AWS_COGNITO_USER_POOL_ID || '';
+// const ClientId = process.env.AWS_COGNITO_CLIENT_ID || '';
 
 
-const userPool = new CognitoUserPool({ UserPoolId, ClientId });
-const cognitoUser = (email: string) => new CognitoUser({ Username: email, Pool: userPool });
+// const userPool = new CognitoUserPool({ UserPoolId, ClientId });
+// const cognitoUser = (email: string) => new CognitoUser({ Username: email, Pool: userPool });
 
 
-export function signUp({ email, phoneNumber, password }: SignUpCredential): Promise<ISignUpResult | undefined> {
+// export function signUp({ email, phoneNumber, password }: SignUpCredential): Promise<ISignUpResult | undefined> {
 
-    const attributeList: CognitoUserAttribute[] = [];
+//     const attributeList: CognitoUserAttribute[] = [];
 
-    const attributeEmail = new CognitoUserAttribute({
-        Name: "email",
-        Value: email,
-    })
+//     const attributeEmail = new CognitoUserAttribute({
+//         Name: "email",
+//         Value: email,
+//     })
 
-    attributeList.push(attributeEmail);
+//     attributeList.push(attributeEmail);
 
-    if (phoneNumber) {
-        const attributePhoneNumber = new CognitoUserAttribute({
-            Name: "phone_number",
-            Value: "+959123456789",
-        })
-        attributeList.push(attributePhoneNumber);
-    }
+//     if (phoneNumber) {
+//         const attributePhoneNumber = new CognitoUserAttribute({
+//             Name: "phone_number",
+//             Value: "+959123456789",
+//         })
+//         attributeList.push(attributePhoneNumber);
+//     }
 
-    return new Promise((resolve, reject) => {
-        userPool.signUp(
-            email,
-            password,
-            attributeList,
-            attributeList,
-            (err, result) => {
-                if (err) {
-                    console.log("🚀 ~ signUp ~ err:", err);
-                    reject(err);
-                    return;
-                }
-                resolve(result);
-                console.log("🚀 ~ signUp ~ result:", result);
-            }
-        );
-    })
+//     return new Promise((resolve, reject) => {
+//         userPool.signUp(
+//             email,
+//             password,
+//             attributeList,
+//             attributeList,
+//             (err, result) => {
+//                 if (err) {
+//                     console.log("🚀 ~ signUp ~ err:", err);
+//                     reject(err);
+//                     return;
+//                 }
+//                 resolve(result);
+//                 console.log("🚀 ~ signUp ~ result:", result);
+//             }
+//         );
+//     })
 
-}
-
-
-export function confirmRegistration({ email, otpCode }: ConfirmRegisteration): Promise<any> {
-    const user = cognitoUser(email);
-    return new Promise((resolve, reject) => {
-        user.confirmRegistration(otpCode, true, (err, result) => {
-            if (err) {
-                console.log("🚀 ~ confirmRegistration ~ err:", err);
-                reject(err);
-                return;
-            }
-            resolve(result);
-            console.log("🚀 ~ confirmRegistration ~ result:", result);
-        });
-    });
-}
+// }
 
 
-export function resendConfirmationCode(email: string) {
-    const user = cognitoUser(email);
-    return new Promise((resolve, reject) => {
-        user.resendConfirmationCode((err, result) => {
-            if (err) {
-                console.log("🚀 ~ resendConfirmationCode ~ err:", err);
-                reject(err)
-                return;
-            }
-            resolve(result);
-            console.log("🚀 ~ resendConfirmationCode ~ result:", result);
-        }
-        );
-    })
-}
+// export function confirmRegistration({ email, otpCode }: ConfirmRegisteration): Promise<any> {
+//     const user = cognitoUser(email);
+//     return new Promise((resolve, reject) => {
+//         user.confirmRegistration(otpCode, true, (err, result) => {
+//             if (err) {
+//                 console.log("🚀 ~ confirmRegistration ~ err:", err);
+//                 reject(err);
+//                 return;
+//             }
+//             resolve(result);
+//             console.log("🚀 ~ confirmRegistration ~ result:", result);
+//         });
+//     });
+// }
 
 
-export function signIn({ email, password }: Omit<SignUpCredential, 'phoneNumber'>): Promise<CognitoUserSession | undefined> {
-    const user = cognitoUser(email);
-
-    const authenticationDetails = new AuthenticationDetails({
-        Username: email,
-        Password: password,
-    });
-
-    user.authenticateUser
-
-    return new Promise((resolve, reject) => {
-        user.authenticateUser(authenticationDetails, {
-            onSuccess(session) {
-                resolve(session);
-            },
-            onFailure(err) {
-                reject(err);
-            },
-        });
-    })
-
-}
+// export function resendConfirmationCode(email: string) {
+//     const user = cognitoUser(email);
+//     return new Promise((resolve, reject) => {
+//         user.resendConfirmationCode((err, result) => {
+//             if (err) {
+//                 console.log("🚀 ~ resendConfirmationCode ~ err:", err);
+//                 reject(err)
+//                 return;
+//             }
+//             resolve(result);
+//             console.log("🚀 ~ resendConfirmationCode ~ result:", result);
+//         }
+//         );
+//     })
+// }
 
 
-export async function getOAuth2Token(code: string) {
+// export function signIn({ email, password }: Omit<SignUpCredential, 'phoneNumber'>): Promise<CognitoUserSession | undefined> {
+//     const user = cognitoUser(email);
 
-    const cognitoDomain = process.env.AWS_COGNITO_DOMAIN;
-    const clientId = process.env.AUTH_COGNITO_ID;
-    const clientSecret = process.env.AWS_COGNITO_GOOGLE_IDP_CLIENT_SECRET || '';
-    if (!clientSecret) {
-        throw new Error("Missing AWS_COGNITO_GOOGLE_IDP_CLIENT_SECRET environment variable");
-    }
+//     const authenticationDetails = new AuthenticationDetails({
+//         Username: email,
+//         Password: password,
+//     });
 
-    if (!cognitoDomain || !clientId) {
-        throw new Error(
-            "Missing required environment variables for OAuth authorize URL"
-        );
-    }
+//     user.authenticateUser
 
+//     return new Promise((resolve, reject) => {
+//         user.authenticateUser(authenticationDetails, {
+//             onSuccess(session) {
+//                 resolve(session);
+//             },
+//             onFailure(err) {
+//                 reject(err);
+//             },
+//         });
+//     })
 
-
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`
-    };
-    console.log("🚀 ~ getOAuth2Token ~ headers:", headers)
-
-    const body = new URLSearchParams({
-        redirect_uri: 'http://localhost:3000/login',
-        client_id: clientId as string,
-        code,
-    });
-    console.log("🚀 ~ getOAuth2Token ~ body:", body)
-
-    const res = await fetch(`${cognitoDomain}/oauth2/token`, {
-        method: 'POST',
-        headers,
-        body
-    });
-
-    console.log("🚀 ~ getOAuth2Token ~ res:", res)
-
-}
-
-export function forgotPassword({ email, onSuccess, onFailure, inputVerificationCode }: ForgotPassword) {
-    const user = cognitoUser(email);
-    user.forgotPassword({ onSuccess, onFailure, inputVerificationCode })
-}
+// }
 
 
-export function confirmForgotPassword({ email, otpCode, newPassword, onSuccess, onFailure }: ConfirmForgotPassword) {
-    const user = cognitoUser(email);
-    user.confirmPassword(otpCode, newPassword, { onSuccess, onFailure });
-}
+// export async function getOAuth2Token(code: string) {
+
+//     const cognitoDomain = process.env.AWS_COGNITO_DOMAIN;
+//     const clientId = process.env.AUTH_COGNITO_ID;
+//     const clientSecret = process.env.AWS_COGNITO_GOOGLE_IDP_CLIENT_SECRET || '';
+//     if (!clientSecret) {
+//         throw new Error("Missing AWS_COGNITO_GOOGLE_IDP_CLIENT_SECRET environment variable");
+//     }
+
+//     if (!cognitoDomain || !clientId) {
+//         throw new Error(
+//             "Missing required environment variables for OAuth authorize URL"
+//         );
+//     }
 
 
-export function signOut() {
-    const user = userPool.getCurrentUser();
-    if (user) {
-        user.signOut();
-    }
-}
+
+//     const headers = {
+//         'Content-Type': 'application/x-www-form-urlencoded',
+//         'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`
+//     };
+//     console.log("🚀 ~ getOAuth2Token ~ headers:", headers)
+
+//     const body = new URLSearchParams({
+//         redirect_uri: 'http://localhost:3000/login',
+//         client_id: clientId as string,
+//         code,
+//     });
+//     console.log("🚀 ~ getOAuth2Token ~ body:", body)
+
+//     const res = await fetch(`${cognitoDomain}/oauth2/token`, {
+//         method: 'POST',
+//         headers,
+//         body
+//     });
+
+//     console.log("🚀 ~ getOAuth2Token ~ res:", res)
+
+// }
+
+// export function forgotPassword({ email, onSuccess, onFailure, inputVerificationCode }: ForgotPassword) {
+//     const user = cognitoUser(email);
+//     user.forgotPassword({ onSuccess, onFailure, inputVerificationCode })
+// }
+
+
+// export function confirmForgotPassword({ email, otpCode, newPassword, onSuccess, onFailure }: ConfirmForgotPassword) {
+//     const user = cognitoUser(email);
+//     user.confirmPassword(otpCode, newPassword, { onSuccess, onFailure });
+// }
+
+
+// export function signOut() {
+//     const user = userPool.getCurrentUser();
+//     if (user) {
+//         user.signOut();
+//     }
+// }
